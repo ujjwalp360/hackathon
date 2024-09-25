a987d3c906792243e38f643a8fc1b02eef964828
ujjwalp360 <145079274+ujjwalp360@users.noreply.github.com>
2024-09-25 17:43:18 +0000

    332

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
-
+from streamlit_option_menu import option_menu
+import mysql.connector
+import json
+import requests
+from streamlit_lottie import st_lottie
+import pandas as pd
 st.title("hello")
 st.write("how are you")
 st.set_page_config(page_title="ujj",page_icon="💀")
