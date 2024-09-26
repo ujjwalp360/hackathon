1ddd38d2d49572beee7ab2fc20f9f150f28aada0
ujjwalp360 <ujjwalp360@gmail.com>
2024-09-26 14:38:22 +0000

    vx

--- login.py
+++ login.py
@@ -2,5 +2,5 @@ import streamlit as st
 def loginPage():
     st.title("Login Page")
     if st.button("Go Back to Home"):
-        st.experimental_get_query_params(page="home")
+        st.query_params(page="home")
         st.rerun()
