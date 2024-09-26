828ef090e683e4f0354b20bf20472ffbbd776740
ujjwalp360 <ujjwalp360@gmail.com>
2024-09-26 13:57:36 +0000

    sgyg

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-import mysql.connector
-import json
-import requests
-from streamlit_lottie import st_lottie
-import pandas as pd
 st.set_page_config(page_title="ujj",page_icon="💀")
 
 def go_home():
