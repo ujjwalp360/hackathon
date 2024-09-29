a276e18e87b5a577ce87f0cd9d6e534eecda1216
ujjwalp360 <ujjwalp360@gmail.com>
2024-09-29 04:22:47 +0000

    vds

--- db.py
+++ db.py
@@ -4,7 +4,9 @@ import mysql.connector
 # MySQL Database connection
 def create_db_connection():
     return mysql.connector.connect(
-        host="ber9n6myvypxkq2zmnmt-mysql.services.clever-cloud.com",
-        user="ubd29UcTDrAGhd4nvuhX",
-        database="ber9n6myvypxkq2zmnmt"
+        host="ber9n6myvypxkq2zmnmt-mysql.services.clever-cloud.com",          # Ensure this is your correct host
+        user="ugrgq84wv7mb6riy",      # Correct username here
+        password="ubd29UcTDrAGhd4nvuhX",  # Correct password here
+        database="ber9n6myvypxkq2zmnmt"  # Correct database name
     )
+
