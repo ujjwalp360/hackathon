bf9532f3d7841ccbe589bf284801b73846c42bed
ujjwalp360 <ujjwalp360@gmail.com>
2024-09-29 04:29:34 +0000

    vsdsv

--- login.py
+++ login.py
@@ -1,9 +1,9 @@
 # login.py
 import streamlit as st
 from db import create_db_connection
-from registration import complete_registration
+from registration import complete_registration, get_user_info_ids
 
-# Login functionality
+# Function to log in the user
 def login_user(username, password):
     db = create_db_connection()
     cursor = db.cursor(dictionary=True)
@@ -17,25 +17,33 @@ def login_user(username, password):
     
     return user
 
+# Login page function
 def login_page():
     st.title("Login")
 
     with st.form("login_form"):
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
-        submit_button = st.form_submit_button("Submit")
+        submit_button = st.form_submit_button("Login")
 
     if submit_button:
         user = login_user(username, password)
         
         if user:
             st.success("Login successful!")
-            if user['id'] not in get_user_info_ids():
+
+            # Check if user is registered
+            user_info_ids = get_user_info_ids()
+            if user['id'] not in user_info_ids:
                 st.info("You haven't completed your registration.")
                 if st.button("Complete Registration"):
-                    complete_registration(user['id'])
+                    complete_registration(user['id'])  # Redirect to registration
             else:
                 st.success("You are fully registered.")
-                st.button("Check Eligibility")
+                if st.button("Check Eligibility"):
+                    st.success("Redirecting to eligibility check...")
+                    # Here, you can call the eligibility check function or redirect to its page.
+                    # Example:
+                    # show_eligibility_check(user['id'])  # Assuming this function checks eligibility
         else:
             st.error("Invalid username or password.")
